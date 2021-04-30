749cc9b1a04507b0afc1ff6cf9decdb12ba1fb6e
Kenneth Hernandez Salazar <khken21@gmail.com>
2021-04-29 21:35:19 -0600

    Updates more project Files

--- ProjectFile/ProjectFile_bb.v
+++ ProjectFile/ProjectFile_bb.v
@@ -1,10 +1,22 @@
 
 module ProjectFile (
 	clk_clk,
-	regs_export,
-	reset_reset_n);	
+	interrupbutton_export,
+	reset_reset_n,
+	segment1_export,
+	segment2_export,
+	segment3_export,
+	segment4_export,
+	segment5_export,
+	segment6_export);	
 
 	input		clk_clk;
-	output	[7:0]	regs_export;
+	input		interrupbutton_export;
 	input		reset_reset_n;
+	output	[7:0]	segment1_export;
+	output	[7:0]	segment2_export;
+	output	[7:0]	segment3_export;
+	output	[7:0]	segment4_export;
+	output	[7:0]	segment5_export;
+	output	[7:0]	segment6_export;
 endmodule
--- ProjectFile/ProjectFile_inst.vhd
+++ ProjectFile/ProjectFile_inst.vhd
@@ -1,15 +1,27 @@
 	component ProjectFile is
 		port (
-			clk_clk       : in  std_logic                    := 'X'; -- clk
-			regs_export   : out std_logic_vector(7 downto 0);        -- export
-			reset_reset_n : in  std_logic                    := 'X'  -- reset_n
+			clk_clk               : in  std_logic                    := 'X'; -- clk
+			interrupbutton_export : in  std_logic                    := 'X'; -- export
+			reset_reset_n         : in  std_logic                    := 'X'; -- reset_n
+			segment1_export       : out std_logic_vector(7 downto 0);        -- export
+			segment2_export       : out std_logic_vector(7 downto 0);        -- export
+			segment3_export       : out std_logic_vector(7 downto 0);        -- export
+			segment4_export       : out std_logic_vector(7 downto 0);        -- export
+			segment5_export       : out std_logic_vector(7 downto 0);        -- export
+			segment6_export       : out std_logic_vector(7 downto 0)         -- export
 		);
 	end component ProjectFile;
 
 	u0 : component ProjectFile
 		port map (
-			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
-			regs_export   => CONNECTED_TO_regs_export,   --  regs.export
-			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+			clk_clk               => CONNECTED_TO_clk_clk,               --            clk.clk
+			interrupbutton_export => CONNECTED_TO_interrupbutton_export, -- interrupbutton.export
+			reset_reset_n         => CONNECTED_TO_reset_reset_n,         --          reset.reset_n
+			segment1_export       => CONNECTED_TO_segment1_export,       --       segment1.export
+			segment2_export       => CONNECTED_TO_segment2_export,       --       segment2.export
+			segment3_export       => CONNECTED_TO_segment3_export,       --       segment3.export
+			segment4_export       => CONNECTED_TO_segment4_export,       --       segment4.export
+			segment5_export       => CONNECTED_TO_segment5_export,       --       segment5.export
+			segment6_export       => CONNECTED_TO_segment6_export        --       segment6.export
 		);
 

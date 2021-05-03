45056062369d72b6241fed92ee29a2b26577e476
Kenneth Hernandez Salazar <khken21@gmail.com>
2021-05-03 16:44:04 -0600

    Fix bug of PIO interrupts and add one more

--- ProjectFile/ProjectFile_inst.vhd
+++ ProjectFile/ProjectFile_inst.vhd
@@ -2,6 +2,7 @@
 		port (
 			clk_clk               : in  std_logic                    := 'X'; -- clk
 			interrupbutton_export : in  std_logic                    := 'X'; -- export
+			leds_export           : out std_logic_vector(9 downto 0);        -- export
 			reset_reset_n         : in  std_logic                    := 'X'; -- reset_n
 			segment1_export       : out std_logic_vector(7 downto 0);        -- export
 			segment2_export       : out std_logic_vector(7 downto 0);        -- export
@@ -9,7 +10,7 @@
 			segment4_export       : out std_logic_vector(7 downto 0);        -- export
 			segment5_export       : out std_logic_vector(7 downto 0);        -- export
 			segment6_export       : out std_logic_vector(7 downto 0);        -- export
-			leds_export           : out std_logic_vector(9 downto 0)         -- export
+			hour_changer_export   : in  std_logic                    := 'X'  -- export
 		);
 	end component ProjectFile;
 
@@ -17,6 +18,7 @@
 		port map (
 			clk_clk               => CONNECTED_TO_clk_clk,               --            clk.clk
 			interrupbutton_export => CONNECTED_TO_interrupbutton_export, -- interrupbutton.export
+			leds_export           => CONNECTED_TO_leds_export,           --           leds.export
 			reset_reset_n         => CONNECTED_TO_reset_reset_n,         --          reset.reset_n
 			segment1_export       => CONNECTED_TO_segment1_export,       --       segment1.export
 			segment2_export       => CONNECTED_TO_segment2_export,       --       segment2.export
@@ -24,6 +26,6 @@
 			segment4_export       => CONNECTED_TO_segment4_export,       --       segment4.export
 			segment5_export       => CONNECTED_TO_segment5_export,       --       segment5.export
 			segment6_export       => CONNECTED_TO_segment6_export,       --       segment6.export
-			leds_export           => CONNECTED_TO_leds_export            --           leds.export
+			hour_changer_export   => CONNECTED_TO_hour_changer_export    --   hour_changer.export
 		);
 
